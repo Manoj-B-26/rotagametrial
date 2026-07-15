11c278367bdb313ff9965b4b8e2d8fc48fd8ab10
Manoj-B-26 <155318356+Manoj-B-26@users.noreply.github.com>
2026-07-15 11:53:53 +0000

    Deploying to gh-pages from @ Manoj-B-26/rotagametrial@68b5165403b84fa19934754b3d04c31679f14bb0 🚀

--- js/env.js
+++ js/env.js
@@ -1,9 +1,16 @@
 window.firebaseConfigEnv = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyCf-5gVPvIQEZK9k3Ge-DfRpHalrWZcUc4
+",
+  authDomain: "rotagame.firebaseapp.com
+",
+  projectId: "rotagame
+",
+  storageBucket: "rotagame.firebasestorage.app
+",
+  messagingSenderId: "782822463141
+",
+  appId: "1:782822463141:web:fbbb8a725c80f9ce899f4c
+",
+  measurementId: "G-3D351GC53Q
+"
 };
